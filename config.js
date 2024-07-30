fa01c2d1b92fa66f07fded6a848a7d7106cbc7bf
Arjun7864 <rapidclean2024@gmail.com>
2024-07-30 20:11:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "MTI2NjY4NDU2NDA1NTc4OTYxOQ.GSuN1Q.wnUw5by7JKf3wd4n2KlNCA13IM_iHNN8kWnLkM",
+    channel: "1257958498772324382",
+    messageTemplate: "Hello @everyone, {author} just now uploaded a video {title}!\n{url}",
+    channel_id: "UCBSMHm2EWoZARwCRYQiUu2g",
+    watchInterval: 30000
+}
